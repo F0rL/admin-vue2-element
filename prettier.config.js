50e4a0f5fbbecd6d368340ec87f682cef7471d20
F0rL <kumaloveslife@163.com>
2021-12-20 11:04:09 +0800

    formatter

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: xiongfang
  * @Date: 2021-12-20 10:09:13
- * @LastEditTime: 2021-12-20 10:14:51
+ * @LastEditTime: 2021-12-20 11:02:10
  * @LastEditors: xiongfang
  * @Description: 代码规范
  * @FilePath: \admin-vue2-element\prettier.config.js
