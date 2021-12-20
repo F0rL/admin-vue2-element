e3699157368947c9cfa7943d68608f3245107e7d
F0rL <kumaloveslife@163.com>
2021-12-20 10:18:14 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # admin-vue2-element
-a template for admin pc (vue2, elementUI)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ prettier.config.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: xiongfang
+ * @Date: 2021-12-20 10:09:13
+ * @LastEditTime: 2021-12-20 10:14:51
+ * @LastEditors: xiongfang
+ * @Description: 代码规范
+ * @FilePath: \admin-vue2-element\prettier.config.js
+ */
+
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  htmlWhitespaceSensitivity: 'ignore',
+  vueIndentScriptAndStyle: true,
+  endOfLine: 'lf',
+}
